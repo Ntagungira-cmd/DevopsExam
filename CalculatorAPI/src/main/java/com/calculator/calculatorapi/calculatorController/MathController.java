32bb57101cfe573d278508e062148b848ed04eeb
ntagungira-cmd <ntagungiraali@gmail.com>
2022-12-13 11:02:45 +0200

    integration tests

--- CalculatorAPI/src/main/java/com/calculator/calculatorapi/calculatorController/MathController.java
+++ CalculatorAPI/src/main/java/com/calculator/calculatorapi/calculatorController/MathController.java
@@ -4,6 +4,7 @@ import com.calculator.calculatorapi.CalculatorService.MathOperator;
 import com.calculator.calculatorapi.Dto.DoMathRequest;
 import com.calculator.calculatorapi.Dto.DoMathResponse;
 import com.calculator.calculatorapi.Exceptions.InvalidOperationException;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
